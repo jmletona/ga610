c47d3f2bfabb737f7541826839e86db96dc4faaa
jmletona <jmletona@gmail.com>
2021-07-15 17:30:48 -0600

    no message

--- backend/ga610/src/main/java/com/jmletona/ga610/model/Campus.java
+++ backend/ga610/src/main/java/com/jmletona/ga610/model/Campus.java
@@ -1,13 +1,1 @@
 package com.jmletona.ga610.model;
-
-import javax.persistence.CascadeType;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import java.util.List;
-
-public class Campus {
-
-    /*@OneToMany(cascade= CascadeType.ALL)
-    @JoinColumn(name="id_campus")
-    private List<Person> personList;*/
-}
