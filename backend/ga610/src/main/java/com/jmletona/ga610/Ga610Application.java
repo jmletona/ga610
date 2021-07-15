3ac931222e095d664dbc6faf76a5dd99b6f936a1
jmletona <jmletona@gmail.com>
2021-07-15 12:51:23 -0600

    first commit
    first commit

---
+++ backend/ga610/src/main/java/com/jmletona/ga610/Ga610Application.java
@@ -0,0 +1,13 @@
+package com.jmletona.ga610;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ga610Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ga610Application.class, args);
+	}
+
+}
---
+++ backend/ga610/src/test/java/com/jmletona/ga610/Ga610ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmletona.ga610;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ga610ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
